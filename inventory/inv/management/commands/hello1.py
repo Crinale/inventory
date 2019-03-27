7774e434d9b3715fffa632c856b2239674f355ac
Crinale <devonchu@hotmail.com>
2019-03-26 22:46:03 -0700

    got it to work

--- inventory/inv/management/commands/hello1.py
+++ inventory/inv/management/commands/hello1.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-help = "Say Hello World!"
+	help = "Say Hello World!"
 	
-def handle(self, *args, **options):
-	self.stdout.write("Hello World")
+	def handle(self, *args, **options):
+		print("Hello World")
