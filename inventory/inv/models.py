e91da1f562ced7c7ca0e105af9fdadf8b4b960c0
Crinale <devonchu@hotmail.com>
2019-04-03 21:56:04 -0700

    added models that closely represent what I use on my original code and able to mess with the super admin side and made it so that it can see the class i made from models as an object

--- inventory/inv/models.py
+++ inventory/inv/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+	item = models.CharField(max_length=200)
+	amount = models.IntegerField()
+	url = models.CharField(max_length=200)
+
--- inventory/inventory/settings.py
+++ inventory/inventory/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'inv.apps.InvConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inv',
 ]
 
 MIDDLEWARE = [
